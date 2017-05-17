c4fb0ea794c0d18249f02929a1bcaeabec9602e2
radiantdawn <true.feanor@gmail.com>
2017-05-17 04:46:33 +0300

    DB
    Mess of a db connection.

--- Chattery/archive/13-05-17-main.py
+++ Chattery/archive/13-05-17-main.py
@@ -1,55 +1,27 @@
-import sqlalchemy as sa
 from aiohttp import web
 
 
-db_conf = {'database': 'chattery', 'user': 'postgres', 'password': 'admin',
-           'host': '127.0.0.1', 'port': '5432'}
-
-meta = sa.MetaData()
-
-
-async def init_pg(app):
-    engine = sa.create_engine('postgresql://postgres:admin@127.0.0.1:5432/chattery')
-    app['db'] = engine
-    meta.bind = engine
-
-
-def get_table(t_name, engine):
-    return sa.Table(t_name, meta, autoload=True)
+messages = []
 
 
 def form_msg_list():
     resp = ""
-    conn = app['db'].connect()
-    messages = conn.execute(sa.select([get_table('messages', app['db'])]).order_by('id'))
     for msg in messages:
-        resp = resp + "\r\n\r\n" + msg['username'] + ": " + msg['message']
-    conn.close()
+        resp = resp + "\r\n\r\n" + msg[0] + ": " + msg[1]
     return resp
 
 
 async def get_rlist_handle(request):
     return web.FileResponse("index.html")
 
-
 async def post_msg_handle(request):
     body = await request.post()
-    messages_t = get_table('messages', app['db'])
-    conn = app['db'].connect()
-    coun_t = sa.select([sa.func.count()]).select_from(messages_t)
-    ins = messages_t.insert().\
-        values(id=coun_t,
-               room_id=11,
-               username=body["name"],
-               message=body["msg_text"])
-    conn.execute(ins)
-    conn.close()
+    messages.append((body["name"], body["msg_text"]))
     return web.FileResponse("index.html")
 
-
 async def get_room_handle(request):
+    messages.append(("nobody", "nothing"))
     return web.Response(text=form_msg_list())
-
 
 app = web.Application()
 app.router.add_get('', get_rlist_handle)
@@ -65,7 +37,5 @@ app.router.add_static('/js',
 app.router.add_static('/img',
                       path="E:\workspace\chattery-chat\Chattery/img",
                       name='img')
-
-app.on_startup.append(init_pg)
 
 web.run_app(app, host='127.0.0.1', port=8081)
